1bff6922c3434a9001daead4e9ea4293af2bb64c
sha1337 <seb.hassemer@gmail.com>
2016-02-20 16:02:44 +0100

    Restructured
    Added Timer() and reorganised V2.0.html

--- js/misc.js
+++ js/misc.js
@@ -1,5 +1,11 @@
 Math.TAU = Math.PI *2; // Might be usefull
 
+function isDefined(a){
+    if (typeof a == "undefined")
+        return false;
+    return true;
+}
+
 function getTime(){    // timestamp in seconds since 1970-01-01 00:00:00 UTC.
         //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTime
 		var now = (new Date()).getTime()/1000;
@@ -14,3 +20,47 @@ function generateArray(size,func){
 	
 	return array;
 }
+
+function Timer(){
+	var t		= getTime();
+	var dt		= 0;
+	var running	= false;
+	
+	this.start = function(){
+		if(!running){
+			t = getTime()-dt;
+			running = true;
+		}
+		recalc();
+		return dt;
+	};
+	this.read = function(){
+		recalc()
+		return dt;
+	};
+	var recalc = function(){
+		if (running){
+			dt = getTime()-t;
+		}
+	}
+	this.stop = function(){
+		recalc()
+		running = false;
+		return dt;
+	};
+	this.reset = function(){
+		t		= getTime();
+		dt		= 0;
+		running	= false;
+	}
+	this.isRunning = function(){
+		return running;
+	};
+	this.startStop = function(){
+		if (running)
+			this.stop()
+		else
+			this.stop()
+		return running;
+	};
+}// end Timer()
